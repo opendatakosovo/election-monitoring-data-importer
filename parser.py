ad5e2676cca955c3dff14ad66029d3214e3c995f
legeorges <georges.labreche@gmail.com>
2014-06-18 15:58:49 -0700

    Started parsing ballots observation data

--- parser.py
+++ parser.py
@@ -50,6 +50,25 @@ def parse_csv():
 			how_many_voted_by_four_PM = row[51] # Column name: PV07-16
 			how_many_voted_by_seven_PM = row[52] # Column name: PV07-19
 			
+			# BALLOTS - MUNICIPAL ASSEMBLY ELECTIONS
+			total_ballots_mae = row[101] # PAK01
+			invalid_ballots_in_box_mae = row[102] # PAK02
+			ballots_set_aside_mae = row[103] # PAK03
+			# something = row[104] # PAK04
+				
+			# BALLOTS - MAYOR ELECTIONS
+			total_ballots_me = row[105] # PKK01
+			invalid_ballots_in_box_me = row[106] # PKK02
+			ballots_set_aside_me = row[107] # PKK03
+			bollots_put_in_transaparent_bag = row[108] # PKK04
+			
+			# TODO: Figure out if invalid_ballots_in_box_xxx and ballots_set_aside_xxx are redundant.
+			# If invalid_ballots_in_box_xxx and ballots_set_aside_xxx refer to the same thing then we only need to count (invalid_ballots_in_box_xxx) and not the flag (ballots_set_aside_xxx)
+			
+			#FIXME: When dealing with numbers, set in document as int instead of string.
+			#FIXME: Translate PO/YO to True/False boolean values.
+			#FIXME: Translate mutlti-choice values to english (e.g. Gjithmone to Always)
+			
 			observation = {
 				'pollingStation':{
 					'number': polling_station_number,
@@ -66,7 +85,7 @@ def parse_csv():
 						'ultraVioletControl': ultra_violet_control,
 						'identifiedWithDocument': identified_with_document,
 						'fingerSprayed': finger_sprayed,
-						'sealedBallot': sealed_ballot, #FIXME: Store as int instead of string.
+						'sealedBallot': sealed_ballot,
 						'howManyVotedBy':{
 							'tenAM': how_many_voted_by_ten_AM,
 							'onePM': how_many_voted_by_one_PM,
@@ -74,9 +93,27 @@ def parse_csv():
 							'sevenPM': how_many_voted_by_seven_PM
 						}
 					}
+				},
+				'ballots':{
+					'municipalAssembly':{
+						'total': total_ballots_mae,
+						'invalid':{
+							'inBallotBox': invalid_ballots_in_box_mae,
+							'setAside': ballots_set_aside_mae
+						}
+					},
+					'mayoral':{
+						'total': total_ballots_me,
+						'invalid':{
+							'inBallotBox': invalid_ballots_in_box_me,
+							'setAside': ballots_set_aside_me
+						},
+						'putInTransparentBag': bollots_put_in_transaparent_bag
+					}
 				}
 			}
 			
+			# Insert document
 			collection.insert(observation)
 
 parse_csv()	
