425175e8793abcf4b0cc3f9a7c06bcbd9047f505
legeorges <georges.labreche@gmail.com>
2014-08-31 13:37:38 -0700

    Fancy terminal output

--- emdi/__init__.py
+++ emdi/__init__.py
@@ -1,6 +1,7 @@
 import os
 import argparse
 from utils.utils import Utils
+from termcolor import colored
 
 from importers.dia_importer_2013 import DiaImporter2013
 from importers.dia_importer_2014 import DiaImporter2014
@@ -29,28 +30,32 @@ def import_data():
 	collection_name = utils.get_collection_name(election_year, election_type, election_round)
 
 	if os.path.isfile(csv_filepath):
-		print 'Importing observation data from "%s" CSV file to "%s" MongoDB collection...' % (csv_filepath, collection_name)
+		print 'Importing from %s' % csv_filepath,
 
-		importer = None
+		try:
+			importer = None
 
-		# FIXME: Implement AbstractFactory pattern to instanciate appropriate importer object
-		if organization == 'dia':
-			if election_year == '2013':
-				# FIXME: Passing utils as constructor argument because for some reason when
-				# we import it from DiaImporter2013 we get this error message:
-				# 		AttributeError: 'module' object has no attribute 'to_boolean'
-				#
-				# WHY?!?!
-				importer = DiaImporter2013(csv_filepath, collection_name, utils)
+			# FIXME: Implement AbstractFactory pattern to instanciate appropriate importer object
+			if organization == 'dia':
+				if election_year == '2013':
+					# FIXME: Passing utils as constructor argument because for some reason when
+					# we import it from DiaImporter2013 we get this error message:
+					# 		AttributeError: 'module' object has no attribute 'to_boolean'
+					#
+					# WHY?!?!
+					importer = DiaImporter2013(csv_filepath, collection_name, utils)
 
-			elif election_year == '2014':
-				importer = DiaImporter2014(csv_filepath, collection_name, utils)
+				elif election_year == '2014':
+					importer = DiaImporter2014(csv_filepath, collection_name, utils)
 
-		# Run the importer
-		num_of_docs_created = importer.execute()
+			# Run the importer
+			num_of_docs_created = importer.execute()
 
-		print 'Done. Imported %s observation documents.' % num_of_docs_created
+			print colored('		[OK]', 'green', attrs=['bold'])
 
+		except:
+			print colored('		[FAIL]', 'red', attrs=['bold'])
+			raise
 	else:
-		print 'No CSV data file exists for those given arguments.'
+		print colored('INPUT ERROR: No CSV data file exists for those given parameters.', 'red')
 	
